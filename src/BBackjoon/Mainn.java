65aff0f5a847668de6bf8878a3d4921aa7bef517
bpv834 <81566093+bpv834@users.noreply.github.com>
2021-07-21 22:15:55 +0900

    나머지 개수 구하기 실패

--- src/BBackjoon/Mainn.java
+++ src/BBackjoon/Mainn.java
@@ -10,14 +10,24 @@ public class Mainn {
 		Scanner sc=new Scanner(System.in);
 		int arr[]=new int[10];
 		int x[]=new int[10];
-		for(int i=0;i<10;i++) {
-			arr[i]=sc.nextInt();
-			x[i]=arr[i]%42;
-			 
-			
+		int cnt=0;
+		
+		for(int t=0;t<10;t++) {
+			arr[t]=sc.nextInt();
+			x[t]=arr[t]%42;	
 		}
 		System.out.println(Arrays.toString(x));
 		
+		for(int i=0;i<10;i++) {
+		for(int j=1;j<10;j++){
+			
+			if(x[i]==x[j]) {
+				cnt++;
+			}
+		}
+		}	
+		
+		System.out.println(cnt);
 			
 
 	}
---
+++ src/BBackjoon/remains.java
@@ -0,0 +1,49 @@
+package BBackjoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class remains {
+
+	public static void main(String[] args) {
+		System.out.println("정수 10개 입력하시오");
+			Scanner sc=new Scanner(System.in);
+			int arr[]=new int[10];
+			int x[]=new int[10];
+			int cnt=1;
+			int cnt1=1;
+			
+			for(int t=0;t<10;t++) {
+				arr[t]=sc.nextInt();
+				x[t]=arr[t]%42;	
+			}
+			System.out.println(Arrays.toString(x));
+			
+			for(int i=0;i<1;i++) {
+			for(int j=1;j<10;j++){
+				
+				if(x[i]!=x[j]) {
+					cnt++;
+				}
+			}
+			}	
+			for(int i=0;i<1;i++) {
+				for(int j=1;j<10;j++){
+					
+					if(x[i]==x[j]) {
+						cnt1++;
+					}
+				}
+				}	
+			
+			System.out.println(cnt+""+cnt1);
+			
+				
+
+		
+
+	}
+
+
+	}
+
